956cff404688f85f6ef63259b23f12a46731aad4
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-04 12:35:15 +0600

    create toast hook for use toast

---
+++ src/hooks/useToast.ts
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export const useToast = () => {
+  const [toast, setToast] = useState<{
+    message: string;
+    type: "success" | "error" | "info";
+  } | null>(null);
+
+  const showToast = (message: string, type: "success" | "error" | "info") => {
+    setToast({ message, type });
+  };
+
+  const hideToast = () => {
+    setToast(null);
+  };
+
+  return { toast, showToast, hideToast };
+};
