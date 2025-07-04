c28736da30df09ac09876099969732fd07f5ba95
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-04 12:23:21 +0600

    create book api

---
+++ src/api/bookApi.ts
@@ -0,0 +1,58 @@
+import { baseApi } from "./baseApi";
+import { IBook, BookListResponse } from "../types/types";
+
+export const bookApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getBooks: builder.query<
+      BookListResponse,
+      { page?: number; limit?: number; filter?: string }
+    >({
+      query: ({ page = 1, limit = 10, filter }) =>
+        `/books?page=${page}&limit=${limit}${
+          filter ? `&filter=${filter}` : ""
+        }`,
+      providesTags: ["Book"],
+    }),
+    getBook: builder.query<IBook, string>({
+      query: (id) => `/books/${id}`,
+      providesTags: (result, error, id) => [{ type: "Book", id }],
+    }),
+    createBook: builder.mutation<IBook, Partial<IBook>>({
+      query: (body) => ({
+        url: "/books",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Book"],
+    }),
+    updateBook: builder.mutation<
+      IBook,
+      { id: string; changes: Partial<IBook> }
+    >({
+      query: ({ id, changes }) => ({
+        url: `/books/${id}`,
+        method: "PUT",
+        body: changes,
+      }),
+      invalidatesTags: (result, error, { id }) => [
+        { type: "Book", id },
+        "Book",
+      ],
+    }),
+    deleteBook: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/books/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Book"],
+    }),
+  }),
+});
+
+export const {
+  useGetBooksQuery,
+  useGetBookQuery,
+  useCreateBookMutation,
+  useUpdateBookMutation,
+  useDeleteBookMutation,
+} = bookApi;
