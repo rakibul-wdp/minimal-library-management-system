b36965c9fc71711f1d324dd8935591dbcccf22da
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-04 12:23:44 +0600

    create borrow api

---
+++ src/api/borrowApi.ts
@@ -0,0 +1,24 @@
+import { baseApi } from "./baseApi";
+import { IBorrow, BorrowSummary } from "../types/types";
+
+export const borrowApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    borrowBook: builder.mutation<
+      IBorrow,
+      Omit<IBorrow, "_id" | "createdAt" | "updatedAt">
+    >({
+      query: (body) => ({
+        url: "/borrow",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Borrow", "Book"],
+    }),
+    getBorrowSummary: builder.query<BorrowSummary[], void>({
+      query: () => "/borrow",
+      providesTags: ["Borrow"],
+    }),
+  }),
+});
+
+export const { useBorrowBookMutation, useGetBorrowSummaryQuery } = borrowApi;
